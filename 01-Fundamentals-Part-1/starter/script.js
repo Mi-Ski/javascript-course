cbdd265ae96ba020b57fdf96c3feff5bb4e54aaa
Mic-PC <skibamichal@interia.pl>
2021-01-18 21:08:52 +0100

    18.01.21

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -36,7 +36,9 @@
 
 // centuryCalculator(2137);
 
-// type conversion/type coercion differences
+// * type conversion/type coercion differences
+// * type coersion: implicit, behind the scenes
+// * type conversion: explicit
 
 // const year = "1991";
 // console.log(Number(year) + 12);
@@ -44,14 +46,62 @@
 // console.log(Number(letter));
 // console.log(`${year} ${Number(year)}`);
 
+// * can only convert to number, string and boolean
+
 // console.log(typeof Number(letter));
 // console.log(typeof NaN);
 // console.log(String(NaN), NaN);
 
-// console.log('23'+'3'+2);
-// console.log('23'-'3'-2);
-// console.log('21' > '');
+// console.log(Number('23'+'3')+2); // 235
+// console.log('23'-'3'-2); // 18
+// console.log('21' > Number('24')); // false
 
-// truthy/falsy values
+// * truthy/falsy values
+// * values that aren't exactly true/false but will become them when converted to booleans
 
-console.log(Boolean());
+// console.log(Boolean());
+
+// * falsy values: 0, '', undefined, null, NaN
+
+// const money = 1;
+// if (money) {
+// 	console.log("don't spend it all");
+// } else {
+// 	console.log('you should get a job');
+// }
+
+// let height = 1;
+// if (height) {
+//    console.log("height is defined");
+// } else {
+//    console.log("height is undefined");
+// }
+
+// * equality operators: == vs ===
+
+// const age = "18";
+// * loose equalty operator with type coercion
+// if (age == 18) console.log("output"); // output
+
+// * strict equality operator, no type coercion
+// if (age === 18) console.log("output"); // no output
+
+// let value = prompt("what's your favourite number?");
+// console.log(typeof Number(value));
+
+// * !== strict version
+// * != loose versin
+
+// ? Basic boolean logic && || !
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense || hasGoodVision); //true
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+if(shouldDrive){
+   console.log("she can drive");
+} else {
+   console.log("she cannot drive");
+}
