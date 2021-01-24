bea44e7e226f8561d0805fdf8af751cb81756612
Mi-Ski <skibamichal@interia.pl>
2021-01-24 14:52:00 +0100

    24.01 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -20,10 +20,10 @@
 
 // ? Function declarations
 
-console.log(ageCalculator1(2000));
-function ageCalculator1(birthYear) {
-   return 2021 - birthYear;
-}
+// console.log(ageCalculator1(2000));
+// function ageCalculator1(birthYear) {
+//    return 2021 - birthYear;
+// }
 
 
 
@@ -31,22 +31,22 @@ function ageCalculator1(birthYear) {
 // * function without a name: an anonymous function
 // ? Difference: Declarations can be called before they're declared in the code  
 
-const calcAge2 = function (birthYear) {
-   return 2021 - birthYear;
-}
-console.log(calcAge2(2345));
+// const calcAge2 = function (birthYear) {
+//    return 2021 - birthYear;
+// }
+// console.log(calcAge2(2345));
 
 
 
 // ? Arrow functions: shorter function expressions
 // * implicit returns, no 'this' keyword
 
-const calcAge3 = birthYear =>  2021 - birthYear;
-console.log(calcAge3(1900));
+// const calcAge3 = birthYear =>  2021 - birthYear;
+// console.log(calcAge3(1900));
 
-const yearsUntilRetirement = (birthYear, retirementAge, firstName) => {
-   const age = 2021 - birthYear;
-   return `${firstName} retires in ${retirementAge - age} years.`; 
-}
-console.log(yearsUntilRetirement(2000, 67, 'Bob'));
-console.log(yearsUntilRetirement(1981, 65, 'Ed'));
+// const yearsUntilRetirement = (birthYear, retirementAge, firstName) => {
+//    const age = 2021 - birthYear;
+//    return `${firstName} retires in ${retirementAge - age} years.`; 
+// }
+// console.log(yearsUntilRetirement(2000, 67, 'Bob'));
+// console.log(yearsUntilRetirement(1981, 65, 'Ed'));
