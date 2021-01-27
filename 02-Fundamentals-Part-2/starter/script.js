ecb905cb5c520a1e5a246db0c26620dda658ee0b
Mi-Ski <skibamichal@interia.pl>
2021-01-27 02:31:49 +0100

    objects - dot/bracket notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -54,25 +54,105 @@
 // ? Arrays
 
 // * the literal syntax
-const friends = ['Michael', 'Steven', 'Peter'];
-console.log(friends);
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
 
-const years = new Array(2312, 2317, 2138, 1889);
-console.log(years);
+// const years = new Array(2312, 2317, 2138, 1889);
+// console.log(years);
 
-friends.push('adh');
-console.log(friends.length);
+// friends.push('adh');
+// console.log(friends.length);
 
-friends.pop();
-console.log(friends.length);
+// friends.pop();
+// console.log(friends.length);
 
-console.log(friends[friends.length - 1]);
+// console.log(friends[friends.length - 1]);
 
-friends[2] = 'Jane';
-console.log(friends);
+// friends[2] = 'Jane';
+// console.log(friends);
 
-// * calculations inside arrays are allowed since it expects expressions
+//  * calculations inside arrays are allowed since it expects expressions
 
-const occupation = 'student';
-const michal = ['Mi', 'Ski', 2021 - 2000, occupation, friends];
-console.log(michal);
+// const occupation = 'student';
+// const michal = ['Mi', 'Ski', 2021 - 2000, occupation, friends];
+// console.log(michal);
+
+// ? Array methods
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+//  * Adding elements
+// console.log(friends.push('Jill'));
+
+// friends.unshift('Alice')
+// console.log(friends);
+
+// * Removing elements
+// friends.pop();
+// console.log(friends.pop()); //returns and deletes Peter
+
+// console.log(friends.shift());
+// console.log(friends);
+
+//  * indexOf, includes
+// friends.push('Alice', 'Peter', 'Jill');
+
+// const indexPeter = friends.indexOf('Peter');
+// console.log(friends);
+// console.log(indexPeter);
+// // returns -1 when the element isn't in the array
+
+// console.log(friends.includes('Peter'));
+// friends.push('21');
+// console.log(friends.includes(21));
+// console.log(friends.includes('peter'));
+// // uses strict equality
+// // returns true/false
+
+// if (friends.includes('Peter')) {
+//    console.log('You have a friend named Peter');
+// }
+
+// / ? Objects
+
+// const michalArray = [
+//    'Mi',
+//    'Ski',
+//    2021 - 2000,
+//    'student',
+//    friends,
+//    ['Mom', 'Dad', 'Sister']
+// ];
+
+// console.log(michalArray);
+
+//  * object literal syntax
+// const michalObject = {
+//    firstName: 'Mi',
+//    lastName: 'Ski',
+//    age: 2021 - 2000,
+//    occupation: michalArray[3],
+//    topFriends: friends,
+//    closeFamily: ['Mom', 'Dad', 'Sister']
+// };
+
+// console.log(michalObject);
+
+// ? Dot vs Bracket notation
+
+const michalObject = {
+   firstName: 'Mi',
+   lastName: 'Ski',
+   age: 2021 - 2000,
+   occupation: 'student',
+   closeFamily: ['Mom', 'Dad', 'Sister']
+};
+
+// * dot notation
+console.log(michalObject.age);
+// * bracket notation
+console.log(michalObject['firstName']); // * <- any expression can be used here
+console.log(michalObject['last' + 'Name']);
+
+const flName = prompt("first or last name?")
+console.log(michalObject[flName + 'Name']);
