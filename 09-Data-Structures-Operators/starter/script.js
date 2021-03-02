73a0abeea0255eea0864f3fce46df5ec9078c5e3
Mic-PC <skibamichal@interia.pl>
2021-03-03 00:52:06 +0100

    03.03

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,8 +4,8 @@ const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
 
   openingHours: {
     thu: {
@@ -26,24 +26,26 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function({starterIndex, mainIndex, time, address}) {
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
     console.log(starterIndex, mainIndex, address, time);
   },
 
-  orderPasta: function(ing1, ing2, ing3) {
+  orderPasta: function (ing1, ing2, ing3) {
     console.log(`here's your pasta made with ${ing1}, ${ing2} and ${ing3}`);
-  }
+  },
 };
 
 // const ingredients = [prompt(`Ingredient 1?`), prompt(`Ingredient 2?`), prompt(`Ingredient 3?`)]
 // restaurant.orderPasta(...ingredients);
 
-// restaurant.orderDelivery({
-//   time: '23:23',
-//   address: 'Long street 23',
-//   mainIndex: 2,
-//   starterIndex: 0
-// })
+
+const order = {
+  time: '23:23',
+  address: 'Long street 23',
+  mainIndex: 2,
+  starterIndex: 0
+}
+restaurant.orderDelivery({...order});
 
 // let [starter1, main1] = restaurant.order(3, 2);
 // console.log(starter1, main1, 'desctructured dishes');
@@ -105,27 +107,77 @@ const restaurant = {
 // console.log(o, c);
 
 // ! The spread operator
-const arr = [7, 8, 9];
-const newArr = [1, 2, 3, ...arr];
-console.log(...newArr);
+// const arr = [7, 8, 9];
+// const newArr = [1, 2, 3, ...arr];
+// console.log(...newArr);
 
-const newMenu = [...restaurant.mainMenu, 'item spread 1', 'item spread 2']
-console.log(newMenu);
+// const newMenu = [...restaurant.mainMenu, 'item spread 1', 'item spread 2']
+// console.log(newMenu);
 
 //useful for making array copies
-const mainMenuCopy = [...restaurant.mainMenu];
-console.log(...mainMenuCopy);
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(...mainMenuCopy);
 
 //joining arrays
-const fullMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(...fullMenu)
+// const fullMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(...fullMenu)
 
 //spread operator works on all iterables
-const str = 'Jonas';
-const letterArray = [...str, ' ', 'S.']
-console.log(letterArray);
+// const str = 'Jonas';
+// const letterArray = [...str, ' ', 'S.']
+// console.log(letterArray);
 
 // * spread with objects
-const restauraneNuevo = {...restaurant};
-const newRestaurant = {restaurant};
-console.log(restauraneNuevo, newRestaurant);
+// const restauraneNuevo = {...restaurant};
+// const newRestaurant = {restaurant};
+// console.log(restauraneNuevo, newRestaurant);
+
+// ! Rest pattern and parameters
+// * destructuring
+// spread
+const arr = [1, 2, ...[3, 4, 5]];
+console.log(arr); // 1,2,3,4,5
+
+//destructuring with Rest syntax
+const [a, b, ...others] = arr;
+console.log(a, b, others); // 1,2, [3,4,5]
+
+//---
+const [pizza, , risotto, ...restOfTheMenu] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, risotto, restOfTheMenu); //skipped elements not included
+
+// objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// * functions
+const add = function(...numbers) {
+  // rest syntax takes multiple arguments and packs them in one array, spreading results so only numbers are logged
+  console.log(...numbers);
+
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(" XXXXXX " + sum);
+}
+
+add(2,3,3);
+add(3);
+add(34,56,7,8,4,2)
+
+const spreadAndRest = [21, 4, 5];
+add(...spreadAndRest);
+
+// ! Short circuiting
+// && ||, and or
+// nullish coalescing operator
+
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// nullish operator looks at nullish values, not false values,
+// nullish: null, undefined, (not 0, '')
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
