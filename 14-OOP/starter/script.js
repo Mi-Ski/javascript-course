db2ee2699b1d70642553ee401b29155658f2c152
Mi-Ski <skibamichal@interia.pl>
2021-06-10 00:57:34 +0200

    09.06

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -243,7 +243,6 @@
 // 	this.speed = speed;
 // };
 
-
 // const EV = function(charge, make, speed) {
 // 	//using EV's parameters in the parent class
 // 	Car.call(this, make, speed);
@@ -252,7 +251,7 @@
 // //linking proto
 // EV.prototype = Object.create(Car.prototype);
 // EV.prototype.chargeBattery = function(chargeTo) {
-// 	this.charge = chargeTo;	 
+// 	this.charge = chargeTo;
 // 	console.log(`${this.make}'s battery is at ${this.charge}%`);
 // }
 // EV.prototype.accelerate = function () {
@@ -266,54 +265,205 @@
 // tesla.chargeBattery(10);
 
 // tesla.accelerate();
-class Person {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.year = birthYear;
+
+// ! Object.create() inheritance
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2021 - this.birthYear);
+//   },
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
+// }
+
+// const steven = Object.create(PersonProto);
+// steven.init('Steven', 2000);
+// steven.calcAge();
+
+// const StudentProto = Object.create(PersonProto);
+
+// StudentProto.init = function(firstName, birthYear, course, school) {
+//   PersonProto.init.call(this, firstName, birthYear);
+//   this.course = course;
+//   this.school = school;
+// }
+
+// StudentProto.introduce = function() {
+//   console.log(`Hi, I'm ${this.firstName} and I go to the ${this.school} school.`);
+// }
+
+// const jay = Object.create(StudentProto);
+// jay.init('jay', 2007, 'compsci', 'ucla');
+
+// const person1 = Object.create(PersonProto);
+// person1.init('Peter', 1994);
+
+// console.log(jay);
+// jay.introduce();
+
+// //! class inheritance
+// class Person {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.year = birthYear;
+//   }
+//   // all methods will be on the prototype, not objects themselves
+//   calcAge() {
+//     const now = new Date().getFullYear();
+// 	console.log(now - this.year);
+//   }
+//   greetings() {
+//     console.log(`Greetings, ${this.fullName}.`);
+//   }
+//   set fullName(name) {
+//     if(name.includes(' ')){
+//       this._fullName = name;
+//     } else console.log('not a full name');
+//   }
+//   get fullName() {
+//     return this._fullName;
+//   }
+//   get age() {
+//     return 2021 - this.year;
+//   }
+//   static hey() {
+// 	  console.log(`Hey!	`);
+//   }
+// }
+
+// // extends keyword links prototypes
+// class Student extends Person {
+// 	//constructor not needed when the subclass has exactly the same properties as the parent class
+// 	constructor(name, birthY, course) {
+// 		// super = constructor funciton of the parent class
+// 		//responsible for creating this keyword in this subclass, needs to happen first
+// 		super(name, birthY);
+// 		this.course = course;
+// 	}
+
+// 	introduce() {
+// 		console.log(`Hi, I'm ${this.fullName}, I'm ${this.age} years old.`);
+// 	}
+
+// 	calcAge() {
+// 		console.log(`${this.fullName} is ${2021 - this.year} yo`);
+// 	}
+// }
+
+// const martha = new Student('Martha ', 2010, 'CompSci');
+// console.log(martha);
+// martha.calcAge();
+
+// class Account {
+// 	// * public fields will be set on every instance, not on the prototype
+//   locale = navigator.language;
+//   // * private fields
+//   #movements =  []; 
+//   #pin;
+//   //pin is being set to private undefined, being set later by the constructor
+
+// 	constructor(owner, currency, pin) {
+// 		this.owner = owner;
+// 		this.currency = currency;
+// 		this.#pin = pin;
+// 		// underscore - proteced property
+// 		// this._movements = [];
+// 		// this.locale = navigator.language;
+// 		console.log(`Thanks for opening a new account, ${this.owner}.`);
+// 	}
+// 	getMovements() {
+// 		return this.#movements;
+// 	}
+//   getPin() {
+//     return this.#pin;
+//   }
+
+// 	//public interface for object methods
+// 	deposit(val) {
+// 		this.#movements.push(val);
+//     return this;
+// 	}
+// 	withdraw(val) {
+// 		this.deposit(-val);
+//     return this;
+// 	}
+
+// 	requestLoan(val) {
+// 		if (this.#approveLoan(val)) {
+// 			this.deposit(val);
+// 			console.log(`Loan of ${val} approved!`);
+//       return this;
+// 		}
+// 	}
+//   //* private methods
+// 	#approveLoan(val) {
+// 		return true;
+// 	}
+
+//   static helper() {
+//     // window.open('https://i.kym-cdn.com/photos/images/newsfeed/001/557/977/7b5.png')
+//   }
+// }
+
+// const acc1 = new Account('Jonas', 'EUR', 2222);
+// // console.log(acc1);
+// console.log(acc1.getPin());
+// Account.helper();
+
+// acc1.deposit(200).requestLoan(10000).withdraw(230).withdraw(2000);
+// console.log(acc1);
+
+//! ev challenge
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
   }
-  // all methods will be on the prototype, not objects themselves
-  calcAge() {
-    const now = new Date().getFullYear();
-	console.log(now - this.year);
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} => ${this.speed}`);
+    return this;
   }
-  greetings() {
-    console.log(`Greetings, ${this.fullName}.`);
+  brake() {
+    this.speed -= 10;
+    console.log(`${this.make} => ${this.speed}`);
+    return this;
   }
-  set fullName(name) {
-    if(name.includes(' ')){
-      this._fullName = name;
-    } else console.log('not a full name');
+  get speedUS() {
+    return this.speed / 1.6;
   }
-  get fullName() {
-    return this._fullName;
-  }
-  get age() {
-    return 2021 - this.year;
-  }
-  static hey() {
-	  console.log(`Hey!	`);
+  set speedFromUS(speedUS) {
+    this.speed = speedUS * 1.6;
   }
 }
 
-// extends keyword links prototypes
-class Student extends Person {
-	//constructor not needed when the subclass has exactly the same properties as the parent class
-	constructor(name, birthY, course) {
-		// super = constructor funciton of the parent class
-		//responsible for creating this keyword in this subclass, needs to happen first
-		super(name, birthY);
-		this.course = course;
-	}
-
-	introduce() {
-		console.log(`Hi, I'm ${this.fullName}, I'm ${this.age} years old.`);
-	}
-
-	calcAge() {
-		console.log(`${this.fullName} is ${2021 - this.year} yo`);
-	}
+class EV extends Car {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make,speed);
+    this.#charge = charge;
+  }
+  accelerate() {
+    this.speed += 10;
+    this.#charge -= 1;
+    console.log(`${this.make} => ${this.speed} (${this.#charge}%)`);
+    return this;
+  }
+  brake() {
+    this.speed -= 10;
+    this.#charge += .5;
+    console.log(`${this.make} => ${this.speed} (${this.#charge}%)`);
+    return this;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    console.log(`${this.make} => charge (${this.#charge}%)`);
+  }
 }
 
-const martha = new Student('Martha ', 2010, 'CompSci');
-console.log(martha);
-martha.calcAge();
+const rivian = new EV('Rivian', 120, 23);
+console.log(rivian.accelerate());
+rivian.accelerate().brake().brake().accelerate();
+console.log(rivian.speedUS);
