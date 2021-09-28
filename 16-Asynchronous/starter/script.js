a5f0e0081dc7520541c349b083659159bb6a4d48
Mi-Ski <skibamichal@interia.pl>
2021-09-28 14:16:20 +0200

    14:16

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,10 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
+const p = document.querySelector(".p");
+setTimeout(function () {
+  p.textContent = "Already red?";
+}, 4000);
+p.style.color = "red";
