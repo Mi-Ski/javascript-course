c3fd2788afd8639f92b2ca89103fb00031c0bf71
Mi-Ski <skibamichal@interia.pl>
2021-10-31 13:11:47 +0100

    31.10

---
+++ 15-Mapty/starter/_geolocation.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+navigator.geolocation.getCurrentPosition(
+	(position) => {
+		console.log(position);
+		const { latitude: lat, longitude: long } = position.coords;
+		console.log(lat, long);
+		// window.open("https://www.google.pl/maps/@100,300")
+	},
+	() => {
+		alert("Could not get your location.");
+	}
+);
+
+const map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	attribution:
+		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([51.5, -0.09])
+	.addTo(map)
+	.bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+	.openPopup();
