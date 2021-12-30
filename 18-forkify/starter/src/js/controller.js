67aa01482b95cecef047441c7d8ac8397e1d37e3
MiSki <skibamichal@interia.pl>
2021-12-30 12:49:32 +0100

    30.12

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,29 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+// API key: 62b48df8-9be6-4fdd-b194-9cd52363a29a
+
+const showRecipe = async () => {
+  try {
+    const result = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await result.json();
+    if (!result.ok) throw new Error(`${data.message} (${result.status})`);
+
+    let {recipe} = data.data;
+
+    recipe = {
+      
+    }
+
+    console.log(recipe);
+    console.log(data);
+
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
